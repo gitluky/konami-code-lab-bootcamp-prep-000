db125eaa522ec0488fdcca22c580db0b99065d3f
gitluky <jluk87@gmail.com>
2017-09-23 23:30:59 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,10 +2,10 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 index = 0;
 
 function init() {
-  document.body.addEventListener('keydown', eventHandler)
+  document.body.addEventListener('keydown', konamiInput)
 }
 
-function eventHandler(e){
+function konamiInput(e){
   const key = parseInt(e.which);
   if (key === code[index]){
     index++;
