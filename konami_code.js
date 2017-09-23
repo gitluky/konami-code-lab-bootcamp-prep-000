dd70ab7a44e68d5d1b216bb3757df2b1bb9ee169
gitluky <jluk87@gmail.com>
2017-09-23 22:25:57 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,7 +1,7 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 index = 0;
 
-function init(e) {
+document.body.addEventListener('keydown', function init(e) {
   // Write your JavaScript code inside the init() function
   const key = parseInt(e.which);
   if (key === code[index]){
@@ -13,6 +13,6 @@ function init(e) {
   else {
     index = 0;
   }
-}
+})
 
-document.body.addEventListener('keydown', init)
+
