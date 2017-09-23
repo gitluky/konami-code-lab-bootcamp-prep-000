81a25907cb799c889850ba4ee011b6a2be30cd64
gitluky <jluk87@gmail.com>
2017-09-23 23:58:07 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -10,7 +10,7 @@ function konamiInput(e){
   if (key === code[index]){
     index++;
     if (index === code.length){
-      alert('Konami Code Enabled');
+      alert('Konami Code Enabled' + e.location);
     }
   }
   else {
